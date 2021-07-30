5984da7d181cb7caee054d999dcb8ac255303dc7
huynhlehoangduc <huynhlehoangduc@gmail.com>
2021-07-31 00:49:46 +0700

    feat: add 404, maintain pages (remove unused code)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MaintainComponent } from './pages/maintain/maintain.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
