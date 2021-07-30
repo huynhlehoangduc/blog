88fadbaa571fa645849b611eca612efe0c419f1a
huynhlehoangduc <huynhlehoangduc@gmail.com>
2021-07-31 01:40:13 +0700

    refactor: admin layout

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,7 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { DashboardOutline, MenuFoldOutline, MenuUnfoldOutline } from '@ant-design/icons-angular/icons';
 
+const icons = [DashboardOutline, MenuFoldOutline, MenuUnfoldOutline];
 
 @NgModule({
   declarations: [
@@ -11,7 +17,11 @@ import { AdminComponent } from './admin.component';
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
-  ]
+    AdminRoutingModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzBreadCrumbModule,
+    NzIconModule.forChild(icons),
+  ],
 })
 export class AdminModule { }
